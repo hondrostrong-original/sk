88d032ac11f34c64aba088404f3ff8f89c81281c
hondrostrong-original <80162319+hondrostrong-original@users.noreply.github.com>
2021-03-08 16:48:44 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,192 @@
+var new_lang = {
+
+"lt0": "INOVATÍVNY ROZVOJ!",
+
+"lt1": "HONDRO<span>STRONG</span>",
+
+"lt2": "V 99% prípadov<span>obnovuje tkanivo</span>poškodenych kĺbov!",
+
+"lt3": "<span>Zastaví bolesť</span>за  za 10 minút.",
+
+"lt4": "Vracia pohyblivosť kĺbov<span>za 14 dní!</span>",
+
+"lt5": "<span>Liečba a podpora kĺbov</span>na mikroúrovni.",
+
+"lt6": "Jedinečná  jedinečná,<span>formula!</span>",
+
+"lt7": "ŠPECIÁLNA PONUKA!",
+
+"lt8": "Zľava konči o:",
+
+"lt9": "hodín",
+
+"lt10": "minút",
+
+"lt11": "sekund",
+
+"lt12": "Vybrať krajinu",
+
+"lt13": "Zadajte meno:",
+
+"lt14": "Zadajte telefónne číslo:",
+
+"lt15": "Ostava:<span class=\»pack_count\">7 ks.</span>",
+
+"lt16": "ZNIČENIE KĹBOV:",
+
+"lt17": "KAŽDÝ Z NAS JE V OBLASTI RIZIKA!",
+
+"lt18": "VEK<span>NAD 35 ROKOV</span>",
+
+"lt19": "KLIMAKTÉRIUM<span>U ŽIEN</span>",
+
+"lt20": "NADVAHA<span>SEDAVÝ ŽIVOTNÝ ŠTYL</span>",
+
+"lt21": "<span>ŽVYŠENA</span>FYZICKA ZAŤAŽ",
+
+"lt22": "INFEKCIE",
+
+"lt23": "ZRANENIA",
+
+"lt24": "KAZDÝ Z TÝCHTO FAKTOROV NIČI CIEVY!",
+
+"lt25": "HONDRO<span>STRONG</span>",
+
+"lt26": "Jediný nanotechnologický prostriedok, ktorý prirodnym spôsobom regeneruje kĺby.",
+
+"lt27": "Hondrostrong -  je prírodný bio-krém, prírodný komplex zosilnený mikrosférami bioaktívnych zložiek.",
+
+"lt28": "Má cielený individuálny účinok na chrupavku a spojivové tkanivo kĺbov, priľahlé svaly a cievy.",
+
+"lt29": "JEDINEČNÝ  LIEK",
+
+"lt30": "ARCHÍVNA REFERENCIA",
+
+"lt31": "Od šesťdesiatych rokov minulého storočia ho používajú astronauti, ktorých kĺby boli vystavené závažným degeneratívnym zmenám v podmienkach beztiaže.",
+
+"lt32": "<span>Liek zabranoval všetky patologické procesy v kĺbovom vaku </span>pri porušeni  metabolických procesov, zvýšenou fyzickou námahou a profesionálnym faktorom patologickej zmeny kĺbov..",
+
+"lt33": "V AKÝCH PRIPADOCH PREDPISUJÚ",
+
+"lt34": "HONDRO<span>STRONG</span>",
+
+"lt35": "Pri akýchkoľvek ochoreniach kĺbového, chrupavkového alebo kostného tkaniva<span>(zápalové a infekčné ochorenia, degeneratívne poškodenia)</span>",
+
+"lt36": "Aby sa zabránilo vzniku ochorení kĺbov<span>(je dokázane, že zdravie a pohyblivosť kĺbov sa s vekom rýchlo znižuje)</span>",
+
+"lt37": "<span>Pre športovcov,</span>pre ľudí s vysokou fyzickou námahou",
+
+"lt38": "So sedavým životným štýlom a nadvahou<span>(čо zvyšuje zaťaženie kĺbov!)</span>",
+
+"lt39": "Po zraneniach a operáciách na kĺby",
+
+"lt40": "OBJEDNAŤ SO ZĽAVOU",
+
+"lt41": "ako to funguje",
+
+"lt42": "HONDRO<span>STRONG</span>",
+
+"lt43": "<span>Okamžite</span>zmierňuje bolesť!",
+
+"lt44": "Zlepšuje pružnosť ciev,<span>a zabespečuje poskytnutie živin kĺbom</span>",
+
+"lt45": "Предотвращает болевой симптом в области суставов, спины,<span>вызванного невротическим, воспалительным, стрессовым факторами</span>",
+
+"lt46": "Zabraňuje bolestivému symptómu v kĺboch a chrbte,<span>ktorý je pôsobený neurotickými, zápalovými faktormi</span>",
+
+"lt47": "Chráni <span>chrupavku</span>",
+
+"lt48": "Odstraňuje infekciu",
+
+"lt49": "Odstraňuje kŕče lýtkového svalstva",
+
+"lt50": "Zabraňuje stagnovaniu<span>a opuchu kĺbov</span>",
+
+"lt51": "TECHNOLÓGIA<br><span>ARTROrevival</span>",
+
+"lt52": "Vďaka najnovšiemu vývoju v oblasti bionanotechnológie, komponenty,<span>компоненты HONDROSTRONG sa veľmi rychlo dostanu do tkanivá poškodených kĺbov a majú 10-násobne</span> vyšší účinok ako všetky existujúce lieky!",
+
+"lt53": "Zložky biokremu sa úplne rozpúšťajú v bunkových tekutinách, čo im pomáha rýchlo sa dostať do postihnutých oblastí, integrovať sa do biomatrice kĺbov a regenerovať ju.",
+
+"lt54": "x10",
+
+"lt55": "už im pomohol",
+
+"lt56": "HONDRO<span>STRONG!</span>",
+
+"lt57": "artritída",
+
+"lt58": "Pred dvoma rokmi som dostal neuspokojivú diagnózu - reumatoidnú artritídu. Neviete si predstaviť, ako som sa bál. Koniec koncov, vždy som vedel, že takáto diagnóza je jednosmerná letenka, pretože stav pri takejto chorobe sa z časom zhoršuje. Doktor ma okamžite uistil a povedal, že artritída sa úspešne lieči HONDROSTRONGOM. Po mesiaci užívania som pocítila výrazné zlepšenie: bolesti zmizli, doktori zaznamenali pozitívnu dynamiku a HONDROSTRONG je vždy na dosah ruky!!",
+
+"lt59": "Martina, 51 rokov",
+
+"lt60": "BOLESŤ V DRIEKU",
+
+"lt61": "Som profesionálny powerlifter, 180kg. je moja pracovna váha. Celý život  som  nemyslel na zdravie lebo som vela trenoval. Dostal som sa do bodu že tesne pred dôležitou súťažou moja chrbtica to prestala zvladat.Akútna bolesť chrbta, opuch, nemohol som robiť nič.  Na radu trenera okamžite som začal používať HONDROSTRONG. Kebyže nepouživam tentoprostriedok, s najväčšou pravdepodobnosťou by som musel skončiť svoju športovú kariéru. Za 2 týždne sa mi podarilo vrátiť chrbát späť do normálu. Teraz sa aktívne rehabilitujem. HONDROSTRONG naozaj pomáha",
+
+"lt62": "Roman, 28 rokov",
+
+"lt63": "zranenie kolena",
+
+"lt64": "Spadol som na koleno. Na vyšetreni sa zistilo ze mam narazene koleno, Na to som nebol pripravený, pretože pri takétakom zraneni som musel prinajmenšom tri týždňe odpočivať na posteli! Na radu miloval som si objednal HONDROSTRONG, vďaka ktorému rýchlo za dva týždne zahojil praskliny a ja som mohol slobodne opustiť more, a teraz odporučam každému tento čarovný prostriedok.",
+
+"lt65": "Lenka, 24 rokov",
+
+"lt66": "DAVAJTE POZOR NA NAPODOBNENINY!",
+
+"lt67": "Ak chcete predísť napodobneninam nízkej kvality, zakúpte si originálny produkt iba na tejto stránke.",
+
+"lt68": "100% KVALITA JE ZABEZPEČENÁ VÝROBCOM!",
+
+"lt69": "ako pracujeme:",
+
+"lt70": "Zľava pri objednavke ihned’ ",
+
+"lt71": "Počkajte, kým konzultant zavolá na potvrdenie objednavky",
+
+"lt72": "Platba až po prijatí tovaru",
+
+"lt73": "INOVATÍVNY PRODUKT!",
+
+"lt74": "HONDRO<span>STRONG</span>",
+
+"lt75": "V 99% prípadov<span>obnoví tkanivo</span>poškodených kĺbov!",
+
+"lt76": "<span>Zastaví bolesť</span>za 10 minút.",
+
+"lt77": "Vracia pohyblivosť kĺbov<span>za 14 dní!</span>",
+
+"lt78": "<span>Zdravie a podpora kĺbov</span>na mikroúrovni.",
+
+"lt79": "Jedinečná patentovaná,<span>formula!</span>",
+
+"lt80": "ŠPECIÁLNA PONUKA!",
+
+"lt81": "zľava konči:",
+
+"lt82": "hodín",
+
+"lt83": "minút",
+
+"lt84": "sekund",
+
+"lt85": "Vybrať krajinu",
+
+"lt86": "Zadajte meno:",
+
+"lt87": "Zadajte telefónne číslo:",
+
+"lt88": "Na sklade zostava:<span class=\"pack_count\">7 ks.</span>",
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
